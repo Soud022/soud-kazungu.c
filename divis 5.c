55cdefafe8871205978058fe713c9a62417b8d69
sod0227 <122601896+Soud022@users.noreply.github.com>
2023-02-04 08:58:06 +0300

    Add files via upload

---
+++ divis 5.c
@@ -0,0 +1,13 @@
+//divisibility test
+#include <stdio.h>
+int main() {
+    int a;
+    printf("/n Enter a value:");
+    scanf("%d",&a);
+if(a%5==0){
+    printf("a is divible by 5");}
+    else{ printf("A is not disible by 5");}
+         
+
+    return 0;
+}
