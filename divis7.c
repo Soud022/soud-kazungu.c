4aee4d9b6251148e6fecdbdaf346d88949ec61d4
sod0227 <122601896+Soud022@users.noreply.github.com>
2023-02-04 09:12:59 +0300

    Add files via upload

---
+++ divis7.c
@@ -0,0 +1,12 @@
+//divisibility test
+#include <stdio.h>
+int main() {
+    int a;
+    printf("Enter a value:");
+scanf("%d",&a);
+if(a%7==0){
+    printf("a is divisible by 7");}
+    else{printf("A is not divible by 7" );
+}
+    return 0;
+}
