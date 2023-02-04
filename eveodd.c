f0dcee40d12a67f0a4c7e6b0e56e320841b44db1
sod0227 <122601896+Soud022@users.noreply.github.com>
2023-02-04 09:24:41 +0300

    Add files via upload

---
+++ eveodd.c
@@ -0,0 +1,12 @@
+//odd or even
+#include <stdio.h>
+int main() {
+    int a;
+    printf("Enter a value:");
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("a is divisible by 2");}
+    else{printf("A is not disible by 2");}
+
+    return 0;
+}
