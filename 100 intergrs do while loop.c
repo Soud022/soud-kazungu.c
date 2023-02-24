6d6b96a897b5eec734068c9beeca35ad32d0a954
sod0227 <122601896+Soud022@users.noreply.github.com>
2023-02-24 13:04:56 +0300

    Add files via upload

---
+++ 100 intergrs do while loop.c
@@ -0,0 +1,13 @@
+//progam to find  100 digit no
+#include <stdio.h>
+
+int main() {
+    int n=0;
+    do
+    {
+        printf("%d ",n);
+        n=n+1;
+    }
+    while(n<=100);
+    return 0;
+}
