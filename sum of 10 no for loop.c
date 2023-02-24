c6cc5e02477fed6f256e69884fdddc6daeb2ee1d
sod0227 <122601896+Soud022@users.noreply.github.com>
2023-02-24 12:41:39 +0300

    Add files via upload

---
+++ sum of 10 no for loop.c
@@ -0,0 +1,11 @@
+//progam to find sum interger of 10 natural no
+#include <stdio.h>
+
+int main() {
+    int i,sum=0;
+    for(i=0;i<=10;i++){
+        sum=sum+i;
+    printf("%d ",i);}
+printf("sum=%d ",sum);
+    return 0;
+}
